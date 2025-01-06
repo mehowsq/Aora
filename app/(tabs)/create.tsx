6222c1d4fac20c599539a4bbe33fad9fdc5eec4a
Michal <michal.skuza@droidsonroids.pl>
2025-01-06 19:55:32 +0100

    Learning how React Native Expo Works

---
+++ app/(tabs)/create.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Create = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl">Create</Text>
+    </View>
+  );
+};
+
+export default Create;
---
+++ components/CustomButton.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { TouchableOpacity, Text } from "react-native";
+
+interface CustomButtonProps {
+  title: string;
+  handlePress: () => void;
+  containerStyles?: string | undefined;
+  textStyles?: string | undefined;
+  isLoading?: boolean | undefined;
+}
+
+const CustomButton = ({
+  title,
+  handlePress,
+  containerStyles,
+  textStyles,
+  isLoading,
+}: CustomButtonProps) => {
+  return (
+    <TouchableOpacity
+      onPress={handlePress}
+      activeOpacity={0.7}
+      className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center  ${containerStyles} ${
+        isLoading ? "opacity-50" : ""
+      }`}
+      disabled={isLoading}
+    >
+      <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
